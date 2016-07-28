f563c67a0c1b36df0cb7b885bc437d53653d258d
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-07-28 16:41:32 -0500

    finishes the ReportCard.java project

--- StartingOutWithJava/src/chapter3/ReportCard.java
+++ StartingOutWithJava/src/chapter3/ReportCard.java
@@ -28,14 +28,55 @@ public class ReportCard {
 		//declare all variables for input and calculation(s)
 		
 		String name;			// holds name of student
-		int numTest;			// holds number of tests taken
-		double score,			// holds total of all the scores
+		int numTest = 0;		// holds number of tests taken
+		double scores = 0,		// holds total of all the scores
 			averageScore;		// holds average score of tests
-		char grade;				// holds the letter grade calculated for student
+		char grade = 'F';		// holds the letter grade calculated for student
 		Scanner keyboard;		// holds input from user keyboard  (needs Scanner import)
 		DecimalFormat format;   // holds the format for the average test score (needs DecimalFormat import)
 		
+		keyboard = new Scanner(System.in);
+		format = new DecimalFormat("#00.00");
 		
+		// get student name
+		System.out.print("Student's name: ");
+		name = keyboard.nextLine();
+		
+		// get all test scores, add them together and get number of tests
+		for( int i = 0; i < 5; i++ ){
+			System.out.print("Enter score for test " + (i + 1) + ": ");
+			scores += keyboard.nextDouble();
+			numTest++;
+		}
+		//calculate the average score
+		averageScore = scores / numTest;
+						
+		// assign letter grade
+		/*
+		 * A: 90 - 100
+		 * B: 80 - 89
+		 * C: 70 - 79
+		 * D: 60 - 69
+		 * F: 0 - 59
+		 */
+		
+		if( 0 <= averageScore && averageScore <= 100 ){
+			if( averageScore >= 90 ){
+				grade = 'A';
+			}else if( averageScore >= 80 ){
+				grade = 'B';
+			}else if( averageScore >= 70 ){
+				grade = 'C';
+			}else if( averageScore >= 60 ){
+				grade = 'D';
+			}else{
+				grade = 'F';
+			}
+		}
+		System.out.println(name + " has an average score of " + format.format(averageScore) + " or a(n) " + 
+		grade + " for the class.");
+		
+		keyboard.close();
 	}
 
 }
