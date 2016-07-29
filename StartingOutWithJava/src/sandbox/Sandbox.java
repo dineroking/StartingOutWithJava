e8dc4d26af4413061a978d1edf021d06a5188174
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-07-28 21:25:51 -0500

    Sandbox update

--- StartingOutWithJava/src/sandbox/Sandbox.java
+++ StartingOutWithJava/src/sandbox/Sandbox.java
@@ -1,5 +1,6 @@
 package sandbox;
 import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
 
 public class Sandbox {
 
@@ -7,22 +8,7 @@ public class Sandbox {
 		
 		//this is a comment to see if it is transmitted to the remote repository
 		
-		int first = 3;
-		double second = 4.5;
-		
-		first = (int)second;
-		
-		//int x = 5, y = 28;
-		float z;
-		//z = (float) (y / x);
-		
-		String name = "Ronny";
-		
-		System.out.println(name.length());
-		
-		//System.out.println(z);
-		
-		double pay;
+		String name = JOptionPane.showInputDialog(null, "Enter name: ");
 		
 		
 		double x = 45678.259;
