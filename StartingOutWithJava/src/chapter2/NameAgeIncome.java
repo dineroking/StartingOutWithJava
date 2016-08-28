19127d2a8e51ad256bb588865c57485722129ffa
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-08-27 20:04:17 -0400

    finishes NameAgeIncome.java challenge

--- StartingOutWithJava/src/chapter2/NameAgeIncome.java
+++ StartingOutWithJava/src/chapter2/NameAgeIncome.java
@@ -3,8 +3,18 @@ package chapter2;
 public class NameAgeIncome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Person ronny = new Person("Ronny", "Rosabal", "Male", 34);  // a new person object
+		ronny.setIncome(90000);
+		
+		String print; 									// the string to print out
+		
+		print = "My name is " + ronny.getfName() + " " + ronny.getlName() + ", my age is " 
+		+ ronny.getAge() + " and\nI hope to earn " + ronny.getIncome() + " per year.";
+		
+		// print string
+		
+		System.out.print(print);
 	}
 
 }
