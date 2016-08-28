069709d175e1c96ad28fbe588ca648be592dd261
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-08-27 20:24:19 -0400

    finishes the NameInitials.java challenge

--- StartingOutWithJava/src/chapter2/NameInitials.java
+++ StartingOutWithJava/src/chapter2/NameInitials.java
@@ -1,10 +1,29 @@
+/*
+ * Challenge: 2. Name and Initials
+ * Date: 08.27.16
+ */
+
 package chapter2;
 
 public class NameInitials {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Person ronny = new Person("Ronny", "Rosabal", "Male", 34);
+		ronny.setmName("Alain");
+		
+		char firstInitial = ronny.getfName().charAt(0); 		// first name initial
+		char middleInitial = ronny.getmName().charAt(0);		// middle name initial
+		char lastInitial = ronny.getlName().charAt(0);			// last name initial
+		
+		// print initials
+		
+		String print;											// string to be printed
+		print = "First Inital:\t" + firstInitial;
+		print += "\nMiddle Initial:\t" + middleInitial;
+		print += "\nLast Initial:\t" + lastInitial;
+		
+		System.out.println(print);
 	}
 
 }
