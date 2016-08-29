679139496c9b01810f88aef20e7c400ed787293b
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-08-29 15:16:40 -0400

    finishes the LandCalculation.java challenge

--- StartingOutWithJava/src/chapter2/LandCalculation.java
+++ StartingOutWithJava/src/chapter2/LandCalculation.java
@@ -1,10 +1,36 @@
+/*
+ * Challenge: 6. Land Calculation
+ * Date: 		 08.29.16
+ */
+
 package chapter2;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class LandCalculation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner keyboard = new Scanner(System.in);			// holds the input made from a keyboard
+		final int ACRE = 43560;								// the size of one acre in square feet
+		double landSize;									// total size in square feet
+		double landAcre;									// total size in acres
+		DecimalFormat sizeFormat;							// format for land size
+		
+		sizeFormat = new DecimalFormat("#,###.00");
+		
+		// get user input
+		
+		System.out.print("Enter the square footage of your land: ");
+		landSize = Double.parseDouble( keyboard.nextLine() );
+		landAcre = landSize / ACRE;
+		
+		// give user the results
+		
+		System.out.println("You have " + sizeFormat.format( landAcre ) + " acres of land.");
+		
+		keyboard.close();
 	}
 
 }
