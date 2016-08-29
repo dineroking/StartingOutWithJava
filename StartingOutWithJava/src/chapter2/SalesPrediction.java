35a71dc70925f571c4a65effcaaf3aca60ddace7
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-08-29 14:42:10 -0400

    finishes the SalesPrediction.java challenge

--- StartingOutWithJava/src/chapter2/SalesPrediction.java
+++ StartingOutWithJava/src/chapter2/SalesPrediction.java
@@ -1,9 +1,29 @@
+/*
+ * Challenge: 5. Sales Prediction
+ * Date: 08.29.16
+ */
+
 package chapter2;
+
+import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
 
 public class SalesPrediction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double sales = 0.0; 						// total sales of the company
+		final double SALES_PERCENT = .62;			// percentage of sales
+		double storeSales; 							// total sales of a particular store
+		DecimalFormat currency;						// the format for the money
+		
+		currency = new DecimalFormat("$###,###.00");
+		
+		sales = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter total sales amount."));
+		
+		storeSales = sales * SALES_PERCENT;
+		
+		JOptionPane.showMessageDialog(null, currency.format(storeSales));
 
 	}
 
