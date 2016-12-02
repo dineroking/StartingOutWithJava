aabd744e0973a7127a79ba5731cba3aeba371974
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-12-02 10:24:08 -0500

    initializes a read and write class for testing

---
+++ StartingOutWithJava/src/chapter11/FileReadWrite.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Ronny Rosabal.
+ * Date:      12/2/16
+ * Objective:
+ */
+package chapter11;
+
+
+public class FileReadWrite {
+
+  // VARIABLES
+
+
+
+  // CONSTRUCTORS
+
+
+
+  // ACCESSORS
+
+
+
+  // MUTATORS
+
+
+
+  // METHODS
+
+
+
+  // OUTPUT
+
+}
---
+++ StartingOutWithJava/src/chapter11/FileReadWriteDemo.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Ronny Rosabal.
+ * Date:      12/2/16
+ * Objective:
+ */
+package chapter11;
+
+
+public class FileReadWriteDemo {
+
+  public static void main( String[] args ) {
+
+  }
+}
