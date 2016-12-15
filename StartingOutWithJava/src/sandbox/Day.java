1178e094efcace63add4dd81d755a1c4a7918876
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-12-14 20:32:37 -0600

    adds Day enum class to sandbox directory

---
+++ StartingOutWithJava/src/sandbox/Day.java
@@ -0,0 +1,10 @@
+package sandbox;
+
+/**
+ * Created by Ronny Rosabal.
+ * Date:      12/14/2016
+ * Objective:
+ */
+public enum Day {
+  SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
