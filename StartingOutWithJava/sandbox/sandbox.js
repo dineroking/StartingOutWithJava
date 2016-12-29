8a1731db1dec2f2585d2cf62bed444f7fb01cfd6
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-12-28 21:17:26 -0600

    updates sandbox files

--- StartingOutWithJava/sandbox/sandbox.js
+++ StartingOutWithJava/sandbox/sandbox.js
@@ -3,3 +3,29 @@
  * Date:      12/27/16
  * Objective: JavaScript from general testing
  */
+
+/**
+ * Person creates a person object
+ * @param first the first name of the person
+ * @param last the last name of the person
+ * @constructor
+ */
+
+var first = "Leslie";
+var last = "Gushin";
+
+function Person( first, last ){
+  this.first = first;
+  this.last = last;
+}
+
+var person1 = new Person( "Ronny", "Rosabal" );
+
+console.log( "person1.last:", person1.last );
+console.log( "typeof person1:", typeof person1 );
+console.log( "person1.constructor:" , person1.constructor );
+console.log( person1 instanceof person1.constructor );
+
+var person2 = new Person( "Christian", "Rosabal" );
+console.log( "global first and last:", first, last );
+console.log( "person2 first and last:", person2.first, person2.last );
