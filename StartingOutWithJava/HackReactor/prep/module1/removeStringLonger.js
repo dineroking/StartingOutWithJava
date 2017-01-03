3e0000b36e54f609e1428ce65b6c273873952d7b
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-01-03 16:35:00 -0600

    finishes the removeStringLonger.js

--- StartingOutWithJava/HackReactor/prep/module1/removeStringLonger.js
+++ StartingOutWithJava/HackReactor/prep/module1/removeStringLonger.js
@@ -4,3 +4,32 @@
  * Objective:
  */
 "use strict";
+
+/*Write a function called "removeStringValuesLongerThan".
+ 
+ Given an number and an object, "removeStringValuesLongerThan" removes any properties on the given object whose values are strings longer than the given number.
+ 
+ var obj = {
+ name: 'Montana',
+ age: 20,
+ location: 'Texas'
+ };
+ removeStringValuesLongerThan(6, obj);
+ console.log(obj); // { age: 20, location: 'Texas' }*/
+
+var obj = {
+  name     : 'Montana',
+  age      : 20,
+  location : 'Texas'
+};
+
+function removeStringValuesLongerThan( num, obj ) {
+  for( var prop in obj ) {
+    if( obj.hasOwnProperty( prop ) && obj[prop].length > num ) {
+      delete obj[prop];
+    }
+  }
+}
+
+removeStringValuesLongerThan( 1, obj );
+console.log( obj );
