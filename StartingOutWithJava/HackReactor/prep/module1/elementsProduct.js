00478c26c8420c02c82ff415dedcac8521679f0e
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-01-19 19:28:34 -0600

    initializes elementsProduct.js

---
+++ StartingOutWithJava/HackReactor/prep/module1/elementsProduct.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Ronny Rosabal.
+ * Date:      1/19/17
+ * Objective:
+ */
+"use strict";
+
+/*
+ Write a function called "computeProductOfAllElements".
+ 
+ Given an array of numbers, "computeProductOfAllElements" returns the products of all the elements in the given array.
+ 
+ Notes:
+ * If given array is empty, it should return 0.
+ 
+ var output = computeProductOfAllElements([2, 5, 6]);
+ console.log(output); // --> 60*/
