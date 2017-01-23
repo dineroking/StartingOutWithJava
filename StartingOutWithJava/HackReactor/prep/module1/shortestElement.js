8d7028a544aa4f9c506f603492f3d99000e81993
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-01-23 09:45:20 -0700

    initializes shortestElement.js

---
+++ StartingOutWithJava/HackReactor/prep/module1/shortestElement.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Ronny Rosabal.
+ * Date:      1/23/17
+ * Objective:
+ */
+"use strict";
+
+/*
+ Write a function called "findShortestElement".
+ 
+ Given an array, "findShortestElement" returns the shortest string within the given array.
+ 
+ Notes:
+ * If there are ties, it should return the first element to appear.
+ * If the given array is empty, it should return an empty string.
+ 
+ var output = findShortestElement(['a', 'two', 'three']);
+ console.log(output); // --> 'a'*/
+
+function findShortestElement( arr ){
+  
+}
