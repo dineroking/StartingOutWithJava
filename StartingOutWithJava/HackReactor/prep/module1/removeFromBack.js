e15044b845b4560753d2ecfe947961eaecd26933
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-01-27 16:03:23 -0600

    finishes removeFromBack.js

---
+++ StartingOutWithJava/HackReactor/prep/module1/removeFromBack.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Ronny Rosabal.
+ * Date:      1/27/2017
+ * Objective:
+ */
+"use strict";
+
+/*
+ Write a function called "removeFromBackOfNew".
+ 
+ Given an array, "removeFromBackOfNew" returns a new array containing all but the last element of the given array.
+ 
+ Notes:
+ * You should be familiar with the 'slice' method.
+ 
+ var arr = [1, 2, 3];
+ var output = removeFromBackOfNew(arr);
+ console.log(output); // --> [1, 2]
+ console.log(arr); // --> [1, 2, 3]*/
+
+function removeFromBackOfNew( arr ) {
+  return arr.slice( 0, arr.length - 1 );
+}
+
+console.log( removeFromBackOfNew( [1, 2, 3] ) );
