587d2c9a829bac0013c1fc85ba1dcec1af75b2b6
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-04-16 08:46:22 -0500

    adds scope-safe example to patterns.js

--- StartingOutWithJava/javascript/books/OOJ/patterns.js
+++ StartingOutWithJava/javascript/books/OOJ/patterns.js
@@ -5,6 +5,7 @@
  */
 "use strict";
 
+// an example of closure
 var person = ( function() {
   var age = 0;
   return {
@@ -14,3 +15,22 @@ var person = ( function() {
     }
   }
 }() );
+
+// example of scope-safe constructor
+
+var name = "ronny";
+
+function Person( name ) {
+  if( this instanceof Person ) {
+    this.name = name;
+  } else {
+    return new Person( name );
+  }
+}
+
+var person1 = new Person( "Christian" );
+var person2 = Person( "Leslie" );
+
+console.log( "global name: ", name );
+console.log( "person1:", person1.name );
+console.log( "person2:", person2.name );
