cbe2c8017864277d808b098bc92cd165418c95f2
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-06-02 15:10:07 -0500

    initializes the FlowWindow.java book exercise

---
+++ StartingOutWithJava/src/chapter12/FlowWindow.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Ronny Rosabal.
+ * Date:      6/2/17
+ * Objective: Practice layout managers.
+ */
+package chapter12;
+
+import javax.swing.*;                               // for the Swing classes
+import java.awt.*;                                  // for the FlowLayout class
+
+public class FlowWindow extends JFrame {
+  
+  // VARIABLES
+  
+  
+  
+  // CONSTRUCTORS
+}
