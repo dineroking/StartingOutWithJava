237e7ad74fcb198cb74090f219a849f5b6787d7b
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-06-03 14:34:27 -0500

    finishes the FlowWindow.java book exercise

--- StartingOutWithJava/src/chapter12/FlowWindow.java
+++ StartingOutWithJava/src/chapter12/FlowWindow.java
@@ -12,7 +12,55 @@ public class FlowWindow extends JFrame {
   
   // VARIABLES
   
+  private int windowWidth;
+  private int windowHeight;
+  JPanel panel;
+  
   
   
   // CONSTRUCTORS
+  
+  public FlowWindow() {
+    this( 300, 100 );
+  }
+  
+  public FlowWindow( int width, int height ) {
+    
+    windowWidth = width;
+    windowHeight = height;
+    
+    // create window
+    setTitle( "Flow Manager" );                   // sets the title of the window
+    setSize( windowWidth, windowHeight );         // sets the size of the window
+    setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );    // sets the action to follow when for close button
+    setLayout( new FlowLayout( FlowLayout.CENTER, 100, 0 ) );              // selects the layout manager to use
+    buildPanel();                                 // creates the panel and its components
+    add( panel );                                 // add the panel to the frame
+    setVisible( true );                           // makes the window visible
+    
+  }
+  
+  private void buildPanel() {
+    
+    // create the panel
+    panel = new JPanel(  );
+    panel.setLayout( new BorderLayout(  ) );
+    
+    // create components
+    JButton button1 = new JButton( "Button 1" );
+    JButton button2 = new JButton( "Button 2" );
+    JButton button3 = new JButton( "Button 3" );
+    
+    // add components to panel
+    panel.add( button1, BorderLayout.NORTH );
+    panel.add( button2, BorderLayout.SOUTH );
+    panel.add( button3, BorderLayout.CENTER );
+    
+  }
+  
+  public static void main( String [] args ) {
+    
+    new FlowWindow(  );
+    
+  }
 }
