04739735cc348320e7d52154a8a314d843bf7425
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-06-03 21:26:50 -0500

    updates skeleton02.js

--- StartingOutWithJava/HackReactor/prep/module2/skeleton02.js
+++ StartingOutWithJava/HackReactor/prep/module2/skeleton02.js
@@ -4,3 +4,23 @@
  * Objective:
  */
 "use strict";
+
+/*
+ An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+ 
+ Write and test a function that determines whether a string is an isogram.
+ 
+ Notes:
+ * Assume your input is only letters.
+ * Assume the empty string is an isogram.
+ * Ignore case.
+ * Follow the pseudocode exactly!
+ * */
+
+function isIsogram( text ) {
+  // add each char to a set
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+  // note: a set drops dup values
+  // thus, to see if all the chars were unique,
+  // check length of text and the size of the set
+}
