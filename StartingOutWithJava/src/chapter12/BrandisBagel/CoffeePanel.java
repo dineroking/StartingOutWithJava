d84a04ee16939252dd8fc19d964212b6ca4262ff
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-06-07 13:15:00 -0500

    finishes the CoffeePanel.java for Brandi's Bagel

--- StartingOutWithJava/src/chapter12/BrandisBagel/CoffeePanel.java
+++ StartingOutWithJava/src/chapter12/BrandisBagel/CoffeePanel.java
@@ -12,16 +12,23 @@ public class CoffeePanel extends JPanel {
   
   // VARIABLES
   
-  private JButton noneButton;                                     // holds the button for the none option
-  private JButton regularButton;                                  // holds the button for the regular coffee option
-  private JButton decafButton;                                    // holds the button for the defac coffee option
-  private JButton cappuccinoButton;                               // holds the button for the cappuccino coffee option
-  private ButtonGroup coffeeGroup;                                // groups all the coffee options together
-  
+  public final double NONE_PRICE = 0.0;                                // the price when none is selected
+  public final double REGULAR_PRICE = 1.25;                            // the price when regular coffee is selected
+  public final double DECAF_PRICE = 1.25;                              // the price when decaf coffee is selected
+  public final double CAPP_PRICE = 2.00;                               // the price when cappuccino coffee is selected
+  private JRadioButton noneButton;                                     // holds the button for the none option
+  private JRadioButton regularButton;                                  // holds the button for the regular coffee option
+  private JRadioButton decafButton;                                    // holds the button for the defac coffee option
+  private JRadioButton cappuccinoButton;
+    // holds the button for the cappuccino coffee option
+  private ButtonGroup coffeeGroup;                                     // groups all the coffee options together
   
   
   // CONSTRUCTORS
   
+  /**
+   * CoffeePanel creates the coffee panel for the order calculator
+   */
   public CoffeePanel() {
     
     // set the layout manager
@@ -29,10 +36,10 @@ public class CoffeePanel extends JPanel {
     
     // create the buttons and buttons group
     coffeeGroup = new ButtonGroup();
-    noneButton = new JButton( "None" );
-    regularButton = new JButton( "Regular coffee" );
-    decafButton = new JButton( "Decaf coffee" );
-    cappuccinoButton = new JButton( "Cappuccino coffee" );
+    noneButton = new JRadioButton( "None" );
+    regularButton = new JRadioButton( "Regular coffee", true );
+    decafButton = new JRadioButton( "Decaf coffee" );
+    cappuccinoButton = new JRadioButton( "Cappuccino coffee" );
     
     // group the coffee buttons
     coffeeGroup.add( noneButton );
@@ -46,10 +53,30 @@ public class CoffeePanel extends JPanel {
     add( decafButton );
     add( cappuccinoButton );
     
+    // add a border to the panel
+    setBorder( BorderFactory.createTitledBorder( "Coffee" ) );
+    
   }
   
   
-  
   // METHODS
+  
+  /**
+   * getCoffeeCost gets the cost of the coffee that was selected in the panel
+   * @return the cost of the selected coffee
+   */
+  public double getCoffeeCost() {
+    
+    double cost = REGULAR_PRICE;        // the cost of coffee. regular is automatically selected
+    
+    if( noneButton.isSelected() ) {
+      cost = NONE_PRICE;
+    } else if( decafButton.isSelected() ) {
+      cost = DECAF_PRICE;
+    } else if( cappuccinoButton.isSelected() ) {
+      cost = CAPP_PRICE;
+    }
+    return cost;
+  }
   
 }
