91ff647db313df3f0572791f4194ce3770627d83
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-06-08 14:02:46 -0500

    finishes the chessBoard.js

--- StartingOutWithJava/javascript/books/EJ/2.Program Sctructure/chessBoard.js
+++ StartingOutWithJava/javascript/books/EJ/2.Program Sctructure/chessBoard.js
@@ -4,3 +4,53 @@
  * Objective: book exercise to create a chessboard
  */
 "use strict";
+
+// VARIABLES
+
+var testBoard = "# \n #";
+
+
+// FUNCTIONS
+
+function buildBoard( columns, rows ) {
+  
+  var board = "";           // holds the pattern of the board
+  
+  // run through each row
+  for( var r = 0; r < rows; r++ ) {
+    
+    // run through each column
+    for( var c = 0; c < columns; c += 2 ) {
+      board += ( r % 2 === 0 ) ? "# " : " #";
+    }
+    // move to the next line
+    board += "\n";
+  }
+  
+  return board;
+}
+
+
+// ASSERTIONS
+
+function assertIsEqual( actual, expected, testName ){
+  
+  var failMessage;            // holds the message when the test fails
+  
+  if( actual === expected ) {
+    
+    // display the passed message
+    console.log( "Test passed" );
+  } else {
+    
+    // create the failed message
+    failMessage += "Failed: " + testName;
+    failMessage += "expected '" + expected;
+    failMessage += "' and got ' " + actual;
+    
+    // display the failed message
+    console.log( failMessage );
+    
+  }
+}
+assertIsEqual( buildBoard( 2, 2 ), testBoard, "builds a chessboard");
