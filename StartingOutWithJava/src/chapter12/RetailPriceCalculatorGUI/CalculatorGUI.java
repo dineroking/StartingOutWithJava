54cf324cc59dea3702b6aa45145d86d88ba046a9
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-06-08 21:02:40 -0500

    Initializes the RetailPriceCalculator book programming challenge

---
+++ StartingOutWithJava/src/chapter12/RetailPriceCalculatorGUI/CalculatorGUI.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Ronny Rosabal.
+ * Date:      6/8/17
+ * Objective: GUI for the Retail Price Calculator
+ */
+package chapter12.RetailPriceCalculatorGUI;
+
+import javax.swing.*;
+
+public class CalculatorGUI extends JFrame {
+  
+}
---
+++ StartingOutWithJava/src/chapter12/RetailPriceCalculatorGUI/UserInputPanel.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Ronny Rosabal.
+ * Date:      6/8/17
+ * Objective:
+ */
+package chapter12.RetailPriceCalculatorGUI;
+
+import javax.swing.*;
+
+public class UserInputPanel extends JPanel {
+  
+}
