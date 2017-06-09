c1065e07bd4f4eaf59fb9784996d83780deaec0d
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-06-09 14:47:58 -0500

    updates the skeleton02.js with comments

--- StartingOutWithJava/HackReactor/prep/module2/skeleton02.js
+++ StartingOutWithJava/HackReactor/prep/module2/skeleton02.js
@@ -18,9 +18,33 @@
  * */
 
 function isIsogram( text ) {
+  
+  return new Set( text ).size === text.length;
   // add each char to a set
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
   // note: a set drops dup values
   // thus, to see if all the chars were unique,
   // check length of text and the size of the set
 }
+
+function assertIsEqual( actual, expected, testName ) {
+  
+  if( actual === expected ) {
+    
+    // message if test passed
+    console.log( "Test passed.");
+  } else {
+    
+    var message;        // holds the message to display to user
+    // create the message to present when the test failed
+    message = "Fail: " + testName;
+    message += ", it expected " + expected;
+    message += " and it got " + actual;
+    
+    // display the message
+    console.log( message );
+    
+  }
+}
+
+assertIsEqual( isIsogram( "rony" ), false, "it checks if a word is a isogram" );
