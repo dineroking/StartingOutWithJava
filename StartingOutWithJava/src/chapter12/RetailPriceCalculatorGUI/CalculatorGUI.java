d7a01a5f5e8b6d8041812c8602577c89bbc2c316
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-06-11 20:13:46 -0400

    finishes the Calculator GUI

--- StartingOutWithJava/src/chapter12/RetailPriceCalculatorGUI/CalculatorGUI.java
+++ StartingOutWithJava/src/chapter12/RetailPriceCalculatorGUI/CalculatorGUI.java
@@ -2,11 +2,134 @@
  * Created by Ronny Rosabal.
  * Date:      6/8/17
  * Objective: GUI for the Retail Price Calculator
+ * Problem:  Create a GUI application where the user enters the wholesale cost of an iem and its markup percentage
+ *           into text fields. THe application should have a button that displays the item's retail price when clicked.
  */
 package chapter12.RetailPriceCalculatorGUI;
 
-import javax.swing.*;
+import javax.swing.*;             // for the Swing classes
+import java.awt.*;                // for the BorderLayout class
+import java.awt.event.*;          // for the ActionListener interface
+import java.text.DecimalFormat;   // for the DecimalFormat class
 
 public class CalculatorGUI extends JFrame {
+  
+  // VARIABLES
+  
+  // panels
+  private UserInputPanel inputPanel;    // the panel for all the user inputs
+  private JPanel buttonsPanel;          // the panel for the calculate and exit buttons
+  
+  // buttons
+  private JButton calculateButton;      // the calculate button
+  private JButton exitButton;           // the exit button
+  
+  // formats
+  DecimalFormat dollar;                 // the format for US dollar currency
+  // TODO: check how to format a percentage
+  DecimalFormat percentage;             // the format to show a decimal as a percentage
+  
+  
+  // CONSTRUCTORS
+  
+  public CalculatorGUI() {
+    
+    // set the layout manager
+    setLayout( new BorderLayout() );
+    
+    // create the window
+    setTitle( "Retail Price Calculator" );
+    setSize( 1000, 300 );
+    setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+    
+    // create panels
+    buildPanels();
+    
+    
+    // add panels to the window
+    add( inputPanel, BorderLayout.NORTH );
+    add( buttonsPanel, BorderLayout.SOUTH );
+    
+    // pack and set the window to visible
+    pack();
+    setVisible( true );
+  }
+  
+  
+  
+  // EVENT HANDLERS
+  
+  private class ButtonListener implements ActionListener {
+    
+    public void actionPerformed( ActionEvent e ) {
+      
+      // product info
+      String productName;       // the name of the product
+      double productPrice;      // the price of the product
+      double markupRate;        // the percentage of price markup
+      double totalPrice;        // the price of the product after the markup
+      
+      // message to be displayed
+      String outputMessage;     // the message to be displayed to the user
+      
+      if( e.getSource() == calculateButton ) {
+        
+        try {
+          
+          // get user input
+          productName = inputPanel.getProductInput();
+          productPrice = Double.parseDouble( inputPanel.getPriceInput() );
+          markupRate = Double.parseDouble( inputPanel.getMarkupInput() )
+                       / 100;   // assumes user enters input in whole numbers
+          
+          // calculate the markup
+          totalPrice = ( productPrice * markupRate ) + productPrice;
+          
+          // initialize currency format
+          dollar = new DecimalFormat( "$#0.00" );
+          
+          // create the message to display
+          outputMessage = productName;
+          outputMessage += "\n\nPrice: " + dollar.format( productPrice );
+          outputMessage += "\nMarkup: " + markupRate;
+          outputMessage += "\nTotal: "
+          
+          
+        } catch( IllegalArgumentException o ) {
+          
+          JOptionPane.showMessageDialog( null, "You did not enter a number." );
+        }
+        
+      } else if ( e.getSource() == exitButton ) {
+        System.exit( 0 );
+      }
+    }
+  }
+  
+  
+  // METHODS
+  
+  // build the calculate and exit panel
+  private void buildPanels() {
+    
+    // create the panels
+    inputPanel = new UserInputPanel();
+    buttonsPanel = new JPanel( );
+    
+    // create the buttons
+    calculateButton = new JButton( "Calculate" );
+    exitButton = new JButton( "Exit" );
+    
+    // add buttons to the panel
+    buttonsPanel.add( calculateButton );
+    buttonsPanel.add( exitButton );
+    
+  }
+  
+  // execute the program
+  public static void main( String[] args ) {
+    
+    new CalculatorGUI();
+  }
   
 }
--- StartingOutWithJava/src/chapter12/RetailPriceCalculatorGUI/UserInputPanel.java
+++ StartingOutWithJava/src/chapter12/RetailPriceCalculatorGUI/UserInputPanel.java
@@ -1,12 +1,74 @@
 /**
  * Created by Ronny Rosabal.
  * Date:      6/8/17
- * Objective:
+ * Objective: The panel for the user input of the price and the markup percentage.
  */
 package chapter12.RetailPriceCalculatorGUI;
 
-import javax.swing.*;
+import javax.swing.*;     // for the Swing classes
+import java.awt.*;        // for FlowLayout class
 
 public class UserInputPanel extends JPanel {
   
+  // VARIABLES
+  
+  // panel
+  private JPanel inputPanel;                // the panel to hold the labels and input
+  
+  // labels
+  private JLabel productLabel;              // the label attached to the name of the product
+  private JLabel priceLabel;                // the label attached to the price of the product
+  private JLabel markupLabel;               // the label attached to the markup percentage of the product
+  
+  // input fields
+  private JTextField productInput;          // the input field for the name of the product
+  private JTextField priceInput;            // the input field for the price of the product
+  private JTextField markupInput;           // the input field for the markup percentage of the product
+  
+  
+  
+  // CONSTRUCTORS
+  
+  public UserInputPanel( ) {
+    
+    // set the layout manager of the panel
+    setLayout( new FlowLayout() );
+    
+    // create labels
+    productLabel = new JLabel( "Enter the product's name:" );
+    priceLabel = new JLabel( "Price:" );
+    markupLabel = new JLabel( "Markup" );
+    
+    // create text fields
+    productInput = new JTextField( 20 );
+    priceInput = new JTextField( 10 );
+    markupInput = new JTextField( 5 );
+    
+    // add components to panel
+    add( productLabel );
+    add( productInput );
+    add( priceLabel );
+    add( priceInput );
+    add( markupLabel );
+    add( markupInput );
+    
+  }
+  
+  
+  // METHODS
+  
+  public String getProductInput() {
+    
+    return  productInput.getText();
+  }
+  
+  public String getPriceInput() {
+    
+    return priceInput.getText();
+  }
+  
+  public String getMarkupInput() {
+    
+    return markupInput.getText();
+  }
 }
