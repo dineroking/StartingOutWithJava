0619eca122bf665cdab0e7577197ce3ef29c0509
Ronny Rosabal <ronnyrosabal@gmail.com>
2017-08-24 15:24:44 -0400

    adds guessTheNumber challenge

---
+++ StartingOutWithJava/codewars/guessTheNumber/guessTheNumber.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Ronny Rosabal.
+ * Date:      8/24/17
+ * Objective:
+ */
+"use strict";
+
+/*
+The Guesser class is set up to generate a random number from 1-1000, and allows 10 guesses to get the number.
+
+  Your task is to write a method 'get_number' (Guesser.prototype.getNumber() in javascript) inside the Guesser class to identify a random number from 1-1000.
+
+You should use the method:
+
+  this.guess(number)
+Which will return either:
+
+"Too high!" If the guess is too high.
+"Too low!" If the guess is too low.
+  or "Correct!" If the guess is correct.
+  You can only call this method 10 times before an exception is raised.*/
